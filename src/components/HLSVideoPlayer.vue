bd265e5e8a51f0cd1dcefc55e1e80f4060597256
Gabriel Reinoso <gabriel.reinoso.1996@gmail.com>
2024-02-20 15:13:51 -0300

    Tabla, colores, reintento

--- src/components/HLSVideoPlayer.vue
+++ src/components/HLSVideoPlayer.vue
@@ -1,81 +1,94 @@
 <template>
-    <div class="bus">
-        <h4>{{ patente }}</h4>
-        <video ref="videoPlayer" class="video-js vjs-default-skin" controls width="345"></video>
-        <video ref="videoPlayer2" class="video-js vjs-default-skin" controls width="345"></video>
-    </div>
-  </template>
-  
-  <script>
-  import videojs from 'video.js';
-  import 'video.js/dist/video-js.css';
-  import 'videojs-contrib-hls';
-  
-  export default {
+  <div class="bus">
+    <h4>{{ patente }}</h4>
+    <video ref="videoPlayer" class="video-js vjs-default-skin" controls width="345"></video>
+    <video ref="videoPlayer2" class="video-js vjs-default-skin" controls width="345"></video>
+  </div>
+</template>
 
-    props:{
+<script>
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
+import 'videojs-contrib-hls';
 
-      patente:String,
-      front:String,
-      inside:String
-    },
+export default {
+  props: {
+    patente: String,
+    front: String,
+    inside: String
+  },
 
+  mounted() {
+    // Inicializamos ambos reproductores
+    this.initializePlayer(this.$refs.videoPlayer, this.inside);
+    this.initializePlayer(this.$refs.videoPlayer2, this.front);
+  },
 
-    mounted() {
-      // Referencia al elemento de video
-      const videoElement = this.$refs.videoPlayer;
-  
-      // Inicializar el reproductor de video con video.js
-      this.player = videojs(videoElement, {
-        techOrder: ['html5', 'hlsjs'], // Prioriza HLS.js si está disponible
-        autoplay: false, // Autoplay del video (true/false)
-        controls: true, // Mostrar controles del video (true/false)
-        sources: [{
-          src: this.inside,
-          type: 'application/x-mpegURL' // Tipo del video
-        }]
-      }, function onPlayerReady() {
-        console.log('Reproductor listo');
-      });
-
-      // Referencia al elemento de video
-      const videoElement2 = this.$refs.videoPlayer2;
-  
-      // Inicializar el reproductor de video con video.js
-      this.player = videojs(videoElement2, {
-        techOrder: ['html5', 'hlsjs'], // Prioriza HLS.js si está disponible
-        autoplay: false, // Autoplay del video (true/false)
-        controls: true, // Mostrar controles del video (true/false)
-        sources: [{
-          src: this.front,
-          type: 'application/x-mpegURL' // Tipo del video
-        }]
-      }, function onPlayerReady() {
-        console.log('Reproductor listo');
-      });
-    },
-    beforeDestroy() {
-      // Destruir el reproductor de video al desmontar el componente
-      if (this.player) {
-        this.player.dispose();
-      }
-      if (this.player2) {
-        this.player2.dispose();
-      }
+  beforeDestroy() {
+    // Dispose para liberar recursos cuando se destruya el complemento
+    if (this.player) {
+      this.player.dispose();
     }
-  };
-  </script>
-  
-  <style>
-  /* Estilos opcionales para el componente de video */
-  .item h4 {
-    display: block;
-    margin: 0;
-    color: white;
+    if (this.player2) {
+      this.player2.dispose();
+    }
+  },
+
+  methods: {
+    initializePlayer(videoElement, source) {
+      const player = videojs(videoElement, {
+        techOrder: ['html5'],
+        autoplay: false,
+        controls: true,
+        sources: [{
+          src: source,
+          type: 'application/x-mpegURL'
+        }]
+      }, () => {
+        console.log('Reproductor listo');
+      });
+
+      // Si hay errores, reintentar
+      player.on('error', () => {
+        console.log('Ocurrio un error, reintentando...');
+        this.retryLoad(player, source);
+      });
+    },
+
+    retryLoad(player, source) {
+      const maxRetryAttempts = 3;
+      let retryCount = 0;
+      const retryDelay = 3000; // 3 seconds
+
+      function loadVideo() {
+        player.src([source, 'application/x-mpegURL']);
+        player.load();
+      }
+
+      function retry() {
+        if (retryCount < maxRetryAttempts) {
+          retryCount++;
+          setTimeout(loadVideo, retryDelay);
+        } else {
+          console.error('Fallo al recargar, reintentando...');
+        }
+      }
+
+      retry();
+    }
   }
-  .item video {
-    display: block;
-    box-sizing: border-box;
-  }
-  </style>
-    
+};
+</script>
+
+<style scoped>
+/* Estilos opcionales para el componente de video */
+.bus h4 {
+  display: inline;
+  margin: 0;
+  color: white;
+}
+.bus video {
+  display: block;
+  box-sizing: border-box;
+}
+</style>
